7a298abea5ee58b0159d5c51142dc75c14f22eec
Eldrian <andieldrian@gmail.com>
2019-07-11 14:58:32 +0700

    myPedo function added

--- PedometerTest/ViewController.swift
+++ PedometerTest/ViewController.swift
@@ -20,49 +20,60 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        startTrackingActivityType()
-        startCountingSteps()
-        startUpdating()
+//        startTrackingActivityType()
+//        startCountingSteps()
+//        startUpdating()
+        myPedo()
     }
     
-    func startTrackingActivityType() {
-        activityManager.startActivityUpdates(to: OperationQueue.main) {
-            [weak self] (activity: CMMotionActivity?) in
-            
-            guard let activity = activity else { return }
-            DispatchQueue.main.async {
-                if activity.walking {
-                    self?.activityTypeLabel.text = "Walking"
-                } else if activity.stationary {
-                    self?.activityTypeLabel.text = "Stationary"
-                } else if activity.running {
-                    self?.activityTypeLabel.text = "Running"
-                } else if activity.automotive {
-                    self?.activityTypeLabel.text = "Automotive"
+//    func startTrackingActivityType() {
+//        activityManager.startActivityUpdates(to: OperationQueue.main) {
+//            [weak self] (activity: CMMotionActivity?) in
+//
+//            guard let activity = activity else { return }
+//            DispatchQueue.main.async {
+//                if activity.walking {
+//                    self?.activityTypeLabel.text = "Walking"
+//                } else if activity.stationary {
+//                    self?.activityTypeLabel.text = "Stationary"
+//                } else if activity.running {
+//                    self?.activityTypeLabel.text = "Running"
+//                } else if activity.automotive {
+//                    self?.activityTypeLabel.text = "Automotive"
+//                }
+//            }
+//        }
+//    }
+    
+//    func startCountingSteps() {
+//        pedometer.startUpdates(from: Date()) {
+//            [weak self] pedometerData, error in
+//            guard let pedometerData = pedometerData, error == nil else { return }
+//
+//            DispatchQueue.main.async {
+//                self?.stepsCountLabel.text = pedometerData.numberOfSteps.stringValue
+//            }
+//        }
+//    }
+    
+//    func startUpdating() {
+//        if CMMotionActivityManager.isActivityAvailable() {
+//            startTrackingActivityType()
+//        }
+//
+//        if CMPedometer.isStepCountingAvailable() {
+//            startCountingSteps()
+//        }
+//    }
+
+    func myPedo() {
+        if CMPedometer.isStepCountingAvailable(){
+            pedometer.startUpdates(from: Date()) { (data, error) in
+                DispatchQueue.main.async { //penting
+                    //code that caused error goes here
+                    self.stepsCountLabel.text = data?.numberOfSteps.stringValue
                 }
             }
         }
     }
-    
-    func startCountingSteps() {
-        pedometer.startUpdates(from: Date()) {
-            [weak self] pedometerData, error in
-            guard let pedometerData = pedometerData, error == nil else { return }
-            
-            DispatchQueue.main.async {
-                self?.stepsCountLabel.text = pedometerData.numberOfSteps.stringValue
-            }
-        }
-    }
-    
-    func startUpdating() {
-        if CMMotionActivityManager.isActivityAvailable() {
-            startTrackingActivityType()
-        }
-        
-        if CMPedometer.isStepCountingAvailable() {
-            startCountingSteps()
-        }
-    }
-
 }
